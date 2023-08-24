f7724ca03aa930a01547a2af09d42162601d4a1b
seunggyu <devkim0702@gmail.com>
2023-08-24 12:18:28 +0900

    refactor to readable code

--- index.js
+++ index.js
@@ -11,130 +11,111 @@ const labelDay = document.querySelector('.day');
 const labelDate = document.querySelector('.date');
 const labelMonthYear = document.querySelector('.month-year');
 
-// Show calendar header (Day, Date, Month-Year)
-const calendarHeader = function () {
+const displayHeader = function () {
   labelDay.textContent = dayNames[day];
   labelDate.textContent = date;
   labelMonthYear.textContent = `${monthNames[month]} ${year}`;
 };
-calendarHeader();
+displayHeader();
 
-// Calculate how many days in a month, then update "numDays"
-let numDays;
-const calcNumDays = function () {
-  [1, 3, 5, 7, 8, 10, 12].includes(month + 1) && (numDays = 31);
-  [4, 6, 9, 11].includes(month + 1) && (numDays = 30);
-  [2].includes(month + 1) && (numDays = 28);
+let numberOfDays;
+const calculateNumberOfDays = function () {
+  [1, 3, 5, 7, 8, 10, 12].includes(month + 1) && (numberOfDays = 31);
+  [4, 6, 9, 11].includes(month + 1) && (numberOfDays = 30);
+  [2].includes(month + 1) && (numberOfDays = 28);
 };
 
-// Calculate "adjNum" (number of empyt days of the starting week). Use "date" and "day".
-const adjArray = [1, 2, 3, 4, 5, 6, 0];
-let adjNum;
-let newDate;
+const emptyDaysArray = [1, 2, 3, 4, 5, 6, 0];
+let numberOfEmptyDays;
 
 const fillCalendar = function () {
-  adjNum = adjArray.at(day - (date % 7));
-  calcNumDays();
+  numberOfEmptyDays = emptyDaysArray.at(day - (date % 7));
+  calculateNumberOfDays();
   document
     .querySelectorAll('.item-date')
     .forEach((cell) => (cell.textContent = ''));
-  for (let i = 1; i <= numDays; i++) {
-    document.querySelector(`.item-${i + adjNum}`).textContent = i;
+  for (let i = 1; i <= numberOfDays; i++) {
+    document.querySelector(`.item-${i + numberOfEmptyDays}`).textContent = i;
   }
 };
 fillCalendar();
 
-// Check if it's the current month
-// YES: red "today" and display current month, date, and day.
-// NO: black all and display date = 1
-const checkCurMonth = function () {
+const checkCurrentMonth = function () {
   if (
     labelMonthYear.textContent ===
     `${monthNames[today.getMonth()]} ${today.getFullYear()}`
   ) {
-    document.querySelector(`.item-${adjNum + today.getDate()}`).style.color =
-      'red';
+    document.querySelector(
+      `.item-${numberOfEmptyDays + today.getDate()}`
+    ).style.color = 'red';
     date = today.getDate();
-    calendarHeader();
+    displayHeader();
   } else {
     document
       .querySelectorAll('.item')
       .forEach((item) => (item.style.color = 'black'));
     date = 1;
-    calendarHeader();
+    displayHeader();
   }
 };
-checkCurMonth();
+checkCurrentMonth();
 
-// btnRight, btnLeft
-const btnRight = document.querySelector('.btn--right');
-const btnLeft = document.querySelector('.btn--left');
+const buttonRight = document.querySelector('.button--right');
+const buttonLeft = document.querySelector('.button--left');
 
-btnRight.addEventListener('click', function () {
-  // Get FIRST "day" and "date" of the new month
-  // Calculate day: new day = prev month's "adjNum" + numDaysMod7
-  day = (adjNum + (numDays % 7)) % 7;
-  // // Update date (FIRST date)
+buttonRight.addEventListener('click', function () {
+  day = (numberOfEmptyDays + (numberOfDays % 7)) % 7;
+
   date = 1;
 
-  // Increase month
   month = (month + 1) % 12;
-  // Increase year
-  if (month === 0) year++;
-  // Update numDays
-  calcNumDays();
-  // //Update header
-  calendarHeader();
-  console.log(`New Month: ${monthNames[month]}/${date}/${dayNames[day]}`);
 
-  //Update calendar cells
+  if (month === 0) year++;
+
+  calculateNumberOfDays();
+
+  displayHeader();
+
   fillCalendar();
 
-  // Check if it's the current month
-  checkCurMonth();
+  checkCurrentMonth();
 });
 
-btnLeft.addEventListener('click', function () {
-  // Get LAST "day" and "date" of the new month
-  // Calcualte day: Use prev month's "adjNum" to find new month's LAST "day"
-  if (adjNum === 0) day = 6;
-  else {
-    day = adjNum - 1;
+buttonLeft.addEventListener('click', function () {
+  if (numberOfEmptyDays === 0) {
+    day = 6;
+  } else {
+    day = numberOfEmptyDays - 1;
   }
 
-  // Decrease month
-  if (month - 1 === -1) month = 11;
-  else month--;
-  // Decrease year
-  if (month === 11) year--;
-  // Update numDays
-  calcNumDays();
-  // Update date (LAST date)
-  date = numDays;
-  // Update header
-  calendarHeader();
-  console.log(`New Month: ${monthNames[month]}/${date}/${dayNames[day]}`);
+  if (month - 1 === -1) {
+    month = 11;
+  } else month--;
 
-  //Update calendar cells
+  if (month === 11) year--;
+
+  calculateNumberOfDays();
+  date = numberOfDays;
+  displayHeader();
+
   fillCalendar();
 
-  // Check if it's the current month
-  checkCurMonth();
+  checkCurrentMonth();
 });
 
-// Last: Display clicked date
 document
   .querySelector('.grid-container')
-  .addEventListener('click', function (e) {
-    if (e.target.classList.contains('item-date')) {
-      const cell = e.target;
+  .addEventListener('click', function (event) {
+    if (event.target.classList.contains('item-date')) {
+      const cell = event.target;
       date = +cell.textContent;
       const dateMod7 = date % 7;
-      const dayMod7 = ((adjNum + dateMod7) % 7) - 1;
+      const dayMod7 = ((numberOfEmptyDays + dateMod7) % 7) - 1;
 
-      if (dayMod7 === -1) day = 6;
-      else day = dayMod7;
+      if (dayMod7 === -1) {
+        day = 6;
+      } else day = dayMod7;
 
-      calendarHeader();
+      displayHeader();
     }
   });
